c724619a3fcd3cfb6427024c042ce036c9908ab9
Elyzavetka <lizamybella@gmail.com>
2022-08-20 18:57:39 +0100

    Add forecast real data in js.

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ let date = now.getDate();
 
 let changeDate = document.querySelector("#date-id");
 changeDate.innerHTML = `${month} ${date}, ${day}`;
+
 //Time
 let hours = now.getHours();
 let minutes = now.getMinutes();
@@ -41,6 +42,52 @@ if (minutes < 10) {
 let changeTime = document.querySelector("#time-id");
 changeTime.innerHTML = `${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+//Forecast
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="img/img1.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "fa08fdd055e22cb26592fb746ab2d10d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 //City
 function displayWeatherCondition(response) {
   console.log(response.data.weather[0].main);
@@ -61,6 +108,8 @@ function displayWeatherCondition(response) {
   } else if (response.data.weather[0].main === "Rain") {
     iconElement.setAttribute("src", `img/img3.png`);
   }
+
+  getForecast(response.data.coord);
 }
 
 function search(event) {
@@ -96,3 +145,4 @@ function showTemeratureCel(event) {
 }
 let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", showTemeratureCel);
+//search("Paris");
